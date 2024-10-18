23b0b758a0564c75e6b7d77b6fb25b35328cacc5
koushikballa006 <koushikballa06@gmail.com>
2024-10-18 19:28:39 +0530

    Result component

---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+const Results = ({ results }) => {
+    if (!results) return <p>No results to display.</p>;
+  
+    return (
+      <div>
+        <h2>Analysis Results:</h2>
+        <p>Total Revenue 5Cr Flag: {results.flags.TOTAL_REVENUE_5CR_FLAG}</p>
+        <p>Borrowing to Revenue Flag: {results.flags.BORROWING_TO_REVENUE_FLAG}</p>
+        <p>ISCR Flag: {results.flags.ISCR_FLAG}</p>
+      </div>
+    );
+  };
