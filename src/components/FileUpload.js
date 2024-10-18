ba5330e5f3dfe694ec75897d25767e458f614ec7
koushikballa006 <koushikballa06@gmail.com>
2024-10-18 19:27:46 +0530

    file upload compomnent

---
+++ src/components/FileUpload.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const FileUpload = ({ onFileUpload }) => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (file) {
+      onFileUpload(file);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileChange} accept=".json" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
